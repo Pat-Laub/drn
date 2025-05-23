a1ebe73e8c35682aa31c2d62bc6649fa5fbc54b5
Patrick Laub <patrick.laub@gmail.com>
2025-05-23 17:49:12 +1000

    Move KernelSHAP_DRN class to separate file

--- src/drn/__init__.py
+++ src/drn/__init__.py
@@ -1,7 +1,8 @@
 # __init__.py for drn package
 from .models import *
 from .distributions import *
-from .interpretability import DRNExplainer, KernelSHAP_DRN
+from .interpretability import DRNExplainer
+from .kernel_shap_explainer import KernelSHAP_DRN
 from .train import train
 from .metrics import crps, quantile_score, quantile_losses, rmse
 from .utils import split_and_preprocess, split_data, preprocess_data
