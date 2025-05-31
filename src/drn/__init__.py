0b60419db98f0845d293cc70207d5c80fce69679
Patrick Laub <patrick.laub@gmail.com>
2025-05-31 10:08:51 +1000

    Fix __all__ for type checkers

--- src/drn/__init__.py
+++ src/drn/__init__.py
@@ -11,3 +11,23 @@ from .utils import (
     preprocess_data,
     replace_rare_categories,
 )
+
+from . import models, distributions
+from .models import __all__ as _models_all
+from .distributions import __all__ as _dist_all
+
+__all__ = [
+    *_models_all,
+    *_dist_all,
+    "DRNExplainer",
+    "KernelSHAP_DRN",
+    "crps",
+    "preprocess_data",
+    "replace_rare_categories",
+    "rmse",
+    "split_and_preprocess",
+    "split_data",
+    "train",
+    "quantile_score",
+    "quantile_losses",
+]  # type: ignore[reportUnsupportedDunderAll]
